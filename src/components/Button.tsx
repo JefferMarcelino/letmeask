cfa7c4c99f2928b4d1a7b7b175a54f9085f0b175
Jeffer Marcelino <jeffersunde72@gmail.com>
2022-04-16 18:30:50 +0200

    Firebase added

---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+function Button() {
+    const [ counter, setCounter] = useState(0)
+
+    const increment = () => {
+        setCounter(counter + 1)
+        console.log(counter)
+    }
+
+    return(
+        <button onClick={ increment }>{counter}</button>
+    )
+}
+
+export default Button
